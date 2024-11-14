91b89013fdb8afe6195fbf32c00801c02b81629e
Linka <Polina153@mail.ru>
2024-11-14 20:45:26 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+       helloWorld();
+       Math.random();
+       Calculator calc = new Calculator();
+       calc.equals(calc);
+       //Building building1 = new Building(5, 2, 5,...);
+    }
+
+    private static void helloWorld(){
+        System.out.println("Hello world!");
+    }
+}
